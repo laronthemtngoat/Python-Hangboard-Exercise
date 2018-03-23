5ea4f513718c0c738a5b0fea7f6779e9e7bb2c72
Laron R Lemon <laronlemon@gmail.com>
2018-03-23 10:14:19 -0600

    git remote add origin https://github.com/laronthemtngoat/Python--Hangboard-Project.git git push -u origin master

---
+++ listprint.py
@@ -0,0 +1,4 @@
+holdList = ["Jug", "Sloper", "Big Pocket", "Big Crimp", "Medium Crimp", "Small Crimp", "2-finger Pocket", "Hard Pinch"]
+
+print "Left Hand: %s" % holdList[6]
+print "Right Hand: %s" % holdList[7]
